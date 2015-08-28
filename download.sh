28f5730a1bfa635ee63fce2fb99f78aad7eb5d08
Calvin Pritchard <pritchard.calvin@gmail.com>
2015-08-27 23:31:30 -0700

    fixed download.sh paths

--- download.sh
+++ download.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 download () {
-	local dirname=$1
+	local dirname="../$1"
 	local reponame=$2
-	if [ ! -d "../$dirname" ]; then
+	if [ ! -d "$dirname" ]; then
 		echo Cloning $dirname
-		git clone $reponame $dirname
+		git clone $reponame "$dirname"
 	fi
 }
 download deployr-docker https://github.com/warmdev/deployr-docker.git
